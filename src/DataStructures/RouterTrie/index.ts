fb560db49effc235b160212c32517dd17a7fe162
jwheeler0424 <jonathan.wheeler87@gmail.com>
2024-01-21 15:49:53 -0800

    added insert logic for router trie

--- src/DataStructures/RouterTrie/index.ts
+++ src/DataStructures/RouterTrie/index.ts
@@ -30,6 +30,11 @@ export default class Trie implements RouterTrie {
     const pathChunks = path.split('/');
     const trailingSlash = path.at(-1) === '/' && path.length > 1;
 
+    // Check if the pathChunks array contains a set of parentheses for regex entries
+    while (pathChunks.indexOf('(') > -1 && pathChunks.indexOf(')') > -1) {
+
+    }
+
     if (pathChunks[0] !== '') {
       throw new Error('Path must start with a /');
     }
@@ -44,13 +49,43 @@ export default class Trie implements RouterTrie {
       const prefix = pathChunks[currentIndex];
       const child = currentNode.children ? currentNode.children.get(prefix) : undefined;
       const isLeaf = currentIndex === pathChunks.length - 1;
-      const isParam = prefix.startsWith(':');
-      const isWildcard = prefix === '*';
-      const isRegex = prefix.startsWith('(') && prefix.endsWith(')');
 
+      // If the child exists and the node is a leaf, throw an error
       if (child && isLeaf) {
         throw new Error('Path already exists');
       }
+
+      // If the child exists and the node is not a leaf, continue
+      if (child && !isLeaf) {
+        currentNode = child;
+        continue;
+      }
+
+      // Check if the prefix is a param, wildcard, or regex
+      const isParam = prefix.startsWith(':');
+      const isWildcard = prefix === '*';
+      const isRegex = prefix.startsWith('(/') && prefix.endsWith('/)');
+
+      // If the child does not exist, create a new node
+      if (!child) {
+        const newNode = new Node();
+        newNode.prefix = isRegex ? prefix.slice(1, -1) : prefix;
+        newNode.size = !isWildcard && !isParam ? prefix.length : 0;
+        newNode.parent = currentNode;
+        newNode.handler = isLeaf ? handler : null;
+        newNode.isLeaf = isLeaf;
+        newNode.isParam = isParam;
+        newNode.isWildcard = isWildcard;
+        newNode.isRegex = isRegex;
+
+        if (!currentNode.children) {
+          currentNode.children = new Map();
+        }
+
+        currentNode.children.set(prefix, newNode);
+      }
+
+  
 
       // Increment the current index
       currentIndex++;
