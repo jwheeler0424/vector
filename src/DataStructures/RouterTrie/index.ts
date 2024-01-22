581d12757920c129e3c9b833573efa8dfae2636c
jwheeler0424 <jonathan.wheeler87@gmail.com>
2024-01-22 09:21:01 -0800

    added helper methods to router trie class: splitPath, isRegex

--- src/DataStructures/RouterTrie/index.ts
+++ src/DataStructures/RouterTrie/index.ts
@@ -26,19 +26,13 @@ export default class Trie implements RouterTrie {
     if (!path) {
       throw new Error('Path is required');
     }
-
-    const pathChunks = path.split('/');
-    const trailingSlash = path.at(-1) === '/' && path.length > 1;
-
-    // Check if the pathChunks array contains a set of parentheses for regex entries
-    while (pathChunks.indexOf('(') > -1 && pathChunks.indexOf(')') > -1) {
-
-    }
-
-    if (pathChunks[0] !== '') {
+    if (path[0] !== '/') {
       throw new Error('Path must start with a /');
     }
+
     method = method.toUpperCase() as HttpMethod;
+    const pathChunks = this.splitPath(path);
+
     
     let currentNode = this.root;
     let currentIndex = 0;
@@ -64,7 +58,7 @@ export default class Trie implements RouterTrie {
       // Check if the prefix is a param, wildcard, or regex
       const isParam = prefix.startsWith(':');
       const isWildcard = prefix === '*';
-      const isRegex = prefix.startsWith('(/') && prefix.endsWith('/)');
+      const isRegex = prefix[0] === '/' ? this.isRegex(prefix) : false;
 
       // If the child does not exist, create a new node
       if (!child) {
@@ -93,5 +87,103 @@ export default class Trie implements RouterTrie {
     
   }
 
-  /* Helper Methods */
+  /* ============================================================================ */
+  /*                                Helper Methods                                */
+  /* ============================================================================ */
+
+  /**
+   * Split Path
+   * ----------------------------------------------------------------------------
+   * Split the path into an array of chunks.
+   * 
+   * @name splitPath
+   * @description
+   * This method splits the path into an array of chunks. The chunks are split by
+   * the forward slash '/' character. The chunks are returned as an array. The 
+   * time complixity of this method is O(n) where n is the length of the path.
+   * 
+   * @param {string} path - The path to split
+   * @returns {string[]} - The array of path chunks
+   * 
+   * @example
+   * const path = `/users/:id/:name/(/d+.* /)/ * /edit`;
+   * const chunks = splitPath(path);
+   * 
+   * console.log(chunks);
+   * // ['users', ':id', ':name', '/ d+.* /', '*', 'edit']
+   */
+  private splitPath(path: string): string[] {
+    // TODO: Add support for trailing slashes
+    const chunks: Array<string> = [];
+    let index = 0;
+    let chunk = '';
+    // pFlag is a flag for parentheses
+    let pFlag = false;
+
+    while (index < path.length) {
+      const char = path[index];
+
+      // Check if the character is a parenthesis and set flag accordingly
+      if (char === '(') {
+        pFlag = true;
+        index++;
+        continue;
+      } else if (char === ')') {
+        pFlag = false;
+        index++;
+        continue;
+      }
+
+      // Check if the character is a slash and the first character of the path
+      if (char === '/' && index === 0) {
+        index++;
+        continue;
+      }
+
+      // Check if the character is a slash and the parentheses flag is false
+      if (char === '/' && !pFlag) {
+        chunks.push(chunk);
+        chunk = '';
+        index++;
+        continue;
+      }
+
+      // Add the character to the chunk
+      chunk += char;
+
+      index++;
+    }
+
+    if (chunk.length > 0) {
+      chunks.push(chunk);
+    }
+
+    return chunks;
+  }
+
+  /**
+   * Is Regex
+   * ----------------------------------------------------------------------------
+   * Check if the pattern is a regex pattern.
+   * 
+   * @name isRegex
+   * @description
+   * This method checks if the pattern is a regex pattern. The pattern is a regex
+   * pattern if it starts with a forward slash '/' and ends with a forward slash
+   * and optional flags '/igmysu'. The time complexity of this method is O(n) 
+   * where n is the length of the pattern.
+   * 
+   * @param {string} pattern - The pattern to check
+   * @returns {boolean} - Whether the pattern is a regex pattern
+   * 
+   * @example
+   * const pattern = '/d+.* /';
+   * 
+   * console.log(isRegex(pattern));
+   * // true
+   */
+  private isRegex(pattern: string): boolean {
+    // Regex flags are i, g, m, u, s, and y (ignoreCase, global, multiline, unicode, dotAll, sticky)
+    return pattern.match(/^\/.*\/[igmysu]*$/) !== null;
+  }
 }
